18277fdf17844af6cd98f7110a50c49c88990083
rohansinha01 <rohansinha01@gmail.com>
2024-10-15 18:01:04 -0400

    Hero

---
+++ src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import styles from "./Hero.module.css"
+import { getImageUrl } from "../../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I&apos;m Rohan</h1>
+        <p className={styles.description}>
+          I am a full-stack developer. If you have any questions, please feel
+          free to reach out to me!
+        </p>
+        <a href="mailto:rohansinha01@gmail.com" className={styles.contactBtn}>Contact Me</a>
+      </div>
+      <img src={getImageUrl("hero/Headshot.JPG")} alt="Hero image of me" className={styles.heroImg} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+      
+    </section>
+  );
+};
